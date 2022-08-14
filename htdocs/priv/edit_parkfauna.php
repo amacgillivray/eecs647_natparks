c196ee4f224969ae4e26cae691a3f00b8d574e99
Andrew MacGillivray <agjmacgill@gmail.com>
2022-08-14 15:41:47 -0500

    Add print_form() to edit_parkfauna

--- htdocs/priv/edit_parkfauna.php
+++ htdocs/priv/edit_parkfauna.php
@@ -35,6 +35,48 @@ function get_entries() : array
     $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
     return $entries;
 }
+
+function print_form()
+{
+    $entries = get_entries();
+
+    print <<<'EPF_FORM_OPEN'
+    <article>
+    <h3>Edit Existing Relationships</h3>
+    <p>You can edit and delete existing Park-Fauna relationships using the fields below and the submit button at the bottom of the page.</p>
+    <form id="editparkfauna" action="#" method="POST">
+    EPF_FORM_OPEN;
+
+
+    print '<form id="editparkfauna" action="#" method="POST">';
+    for ($i=0;$i<sizeof($entries);$i++) {
+        $row = $entries[$i];
+        $park_name = $row['pname'];
+        $fauna_name = $row['name'];
+        $park = $row['id'];
+        $spec = $row['code'];
+        $token = "p_${park}_s_${spec}";
+
+        print <<<LoopInputs1
+        <fieldset id="$token" class="parkfauna">
+            <legend>$park_name -> $fauna_name</legend>
+            <div class="fl-pair fl-pair-text">
+                <label for="${token}_park">Park ID</label>
+                <input id="${token}_park" name="${token}_park" type="text" maxlength="10" value="$park">
+            </div>
+            <div class="fl-pair fl-pair-text">
+                <label for="${token}_spec">Fauna Code</label>
+                <input id="${token}_spec" name="${token}_spec" type="text" maxlength="10" value="$spec">
+            </div>
+            <div class="fl-pair fl-pair-checkbox"> <input id="${token}_del" name="${token}_del" type="checkbox">
+                <label for="${token}_del">Delete Relationship</label></div>
+        </fieldset>
+        LoopInputs1;
+    }
+
+    print '</form></article>';
+}
+
 \eecs647\print_html_opener('Edit Images');
 
 // Show the form to edit parks
