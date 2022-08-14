72ab2385e7ef517fe4b297c11e00f11d857e13a4
Andrew MacGillivray <agjmacgill@gmail.com>
2022-08-14 17:05:08 -0500

    Add handler for edit-mode and misc improvements

--- htdocs/priv/edit_parkfauna.php
+++ htdocs/priv/edit_parkfauna.php
@@ -29,6 +29,86 @@ function get_entries() : array
     return $entries;
 }
 
+function handle_edits()
+{
+    $query = <<<'PF_EDIT'
+    UPDATE eecs647.parkfauna
+    SET id = ?, code = ?
+    WHERE id = ? AND code = ?
+    PF_EDIT;
+
+    $delete = <<<'PF_DELETE'
+    DELETE FROM eecs647.parkfauna WHERE id = ? AND code = ?
+    PF_DELETE;
+
+
+    $keys = array_filter(
+        array_keys($_POST),
+        function($field){
+            return (strpos($field,'p_') !== false);
+        });
+    $keys = array_values($keys);
+    $fields = [];
+    $edits = 0;
+
+    $conn = new PDO('odbc:eecs647');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->beginTransaction();
+    try {
+        for ($i = 0; $i < sizeof($keys); $i++) {
+            $key = $keys[$i];
+            $tgt = $_POST[$key];
+            $e = strrpos($key, '_');
+            $fields[substr($key, 0, $e)][] = $tgt;
+        }
+
+        $keys = array_keys($fields);
+        for ($i = 0; $i < sizeof($fields); $i++)
+        {
+            $old_park = substr(
+                $keys[$i],
+                2,
+                strpos($keys[$i], '_s_')-2
+            );
+
+            $old_fauna = substr(
+                $keys[$i],
+                strpos($keys[$i], '_s_')+3
+            );
+
+            $fieldset = $fields[$keys[$i]];
+
+            # Skip values that have not been changed
+            if ($old_park === $fieldset[0] && $old_fauna === $fieldset[1])
+                continue;
+
+            // TODO - DELETION
+//            if (!isset($fieldset[2]))
+            $stmt = $conn->prepare($query);
+            if ($stmt->execute([$fieldset[0], $fieldset[1], $old_park, $old_fauna]))
+                $edits++;
+        }
+
+    } catch (Exception $e) {
+        $conn->rollBack();
+        setcookie('pgmsg', $e->getMessage(), time()+30, '/');
+        header('Location: /priv/edit_parkfauna.php');
+    }
+    $conn->commit();
+    setcookie('pgmsg', "Successfully modified $edits rows.", time()+30, '/');
+    header('Location: /priv/edit_parkfauna.php');
+}
+
+function handle_add()
+{
+//    $query =
+
+//    $conn = new PDO('odbc:eecs647');
+//    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//    $stmt = $conn->prepare($query);
+//    $stmt->execute();
+}
+
 function print_form()
 {
     $entries = get_entries();
@@ -38,6 +118,7 @@ function print_form()
     <h3>Edit Existing Relationships</h3>
     <p>You can edit and delete existing Park-Fauna relationships using the fields below and the submit button at the bottom of the page.</p>
     <form id="editparkfauna" action="#" method="POST">
+        <input type="hidden" name="edit_handler" value="void"/>
     EPF_FORM_OPEN;
 
 
@@ -66,47 +147,68 @@ function print_form()
         </fieldset>
         LoopInputs1;
     }
-
+    print '<input type="Submit" value="Apply Changes &raquo;">';
     print '</form></article>';
 }
 
-\eecs647\print_html_opener('Edit Images');
-
-// Check Privileges
-if (!\eecs647\authorized_user(auths))
+function do_page()
 {
-    print '<section id="pf">';
-    \eecs647\print_err_privs();
+//    var_dump($_POST);
+
+    if (isset($_POST['add_handler']))
+    {
+        handle_add();
+    }
+
+    if (isset($_POST['edit_handler']))
+    {
+        handle_edits();
+    }
+
+    \eecs647\print_html_opener('Edit Park-Fauna Relationships');
+
+    // Check Privileges
+    if (!\eecs647\authorized_user(auths))
+    {
+        print '<section id="pf">';
+        \eecs647\print_err_privs();
+        print '</section>';
+        \eecs647\print_html_closer();
+        exit();
+    }
+
+    # Todo - Check for POST arguments and handle form submission
+
+    $msg = (isset($_COOKIE['pgmsg'])) ? '<p><b style="color:coral">OPERATION HAD EFFECT:</b><br/>'.$_COOKIE['pgmsg'].'</p>' : '';
+
+    // Show the form to edit parks
+    print <<<PAGEOPENER
+    <section id="pf">
+    <h1>Editing Park-Fauna Relationships</h1>
+    <p>Each Park -> Species Relationship indicates that, within the designated park, the specified species can be found.</p>
+    $msg
+    <article>
+        <form id="new_pf" action="#" method="POST">
+            <input type="hidden" name="add_handler" value="void"/>
+            <h3>Add New Relationship</h3>
+            <p>You can add a new Park-Fauna relationship using these fields and the submit button below. Note that submitting will lose any unsaved changes 
+                the row-editor form below the submit button.</p>
+            <div class="fl-pair fl-pair-text">
+                <label for="newpf_park">Park ID</label>
+                <input id="newpf_park" name="newpf_park" type="text" maxlength="10" value="">
+            </div>
+            <div class="fl-pair fl-pair-text">
+                <label for="newpf_spec">Fauna Code</label>
+                <input id="newpf_spec" name="newpf_spec" type="text" maxlength="10" value="">
+            </div>
+            <input type="Submit" value="Add New Relationship &raquo;">
+        </fieldset>
+    </article>
+    PAGEOPENER;
+    print_form();
     print '</section>';
+
     \eecs647\print_html_closer();
-    exit();
 }
 
-# Todo - Check for POST arguments and handle form submission
-
-// Show the form to edit parks
-print <<<'PAGEOPENER'
-<section id="pf">
-<h1>Editing Park-Fauna Relationships</h1>
-<p>Each Park -> Species Relationship indicates that, within the designated park, the specified species can be found.</p>
-<article>
-    <form id="new_pf" action="#" method="POST">
-        <h3>Add New Relationship</h3>
-        <p>You can add a new Park-Fauna relationship using these fields and the submit button below. Note that submitting will lose any unsaved changes 
-            the row-editor form below the submit button.</p>
-        <div class="fl-pair fl-pair-text">
-            <label for="newpf_park">Park ID</label>
-            <input id="newpf_park" name="newpf_park" type="text" maxlength="10" value="">
-        </div>
-        <div class="fl-pair fl-pair-text">
-            <label for="newpf_spec">Fauna Code</label>
-            <input id="newpf_spec" name="newpf_spec" type="text" maxlength="10" value="">
-        </div>
-        <input type="Submit" value="Add New Relationship &raquo;">
-    </fieldset>
-</article>
-PAGEOPENER;
-print_form();
-print '</section>';
-
-\eecs647\print_html_closer();
+do_page();
