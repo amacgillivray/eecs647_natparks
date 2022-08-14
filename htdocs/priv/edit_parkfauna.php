f116280b04c68585f31acd7035054f9a1b6d3bfa
Andrew MacGillivray <agjmacgill@gmail.com>
2022-08-14 15:05:37 -0500

    Add ParkFauna editing query (2 joins) Gets the park and species name along with the parkfauna entry Will be used to generate form fieldsets that edit the relationships

--- htdocs/priv/edit_parkfauna.php
+++ htdocs/priv/edit_parkfauna.php
@@ -14,6 +14,27 @@ const pfa_cols_editable = [
     ]
 ];
 
+function get_entries() : array
+{
+    $query = <<< 'PF_FETCH'
+    SELECT eecs647.parks.pname, 
+           eecs647.fauna.name,
+           eecs647.parkfauna.id, 
+           eecs647.parkfauna.code 
+    FROM eecs647.parkfauna
+    LEFT JOIN eecs647.parks ON eecs647.parks.id = eecs647.parkfauna.id
+    LEFT JOIN eecs647.fauna ON eecs647.fauna.code = eecs647.parkfauna.code
+    GROUP BY eecs647.parks.pname, eecs647.fauna.name, eecs647.parkfauna.id, eecs647.parkfauna.code
+    ORDER BY parkfauna.id DESC
+    PF_FETCH;
+
+    $conn = new PDO('odbc:eecs647');
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare($query);
+    $stmt->execute();
+    $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $entries;
+}
 \eecs647\print_html_opener('Edit Images');
 
 // Show the form to edit parks
